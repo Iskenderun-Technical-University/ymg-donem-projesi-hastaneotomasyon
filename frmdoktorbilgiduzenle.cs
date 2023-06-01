46ff7f5fe2f936098d6b62177eab453d4e50c3cd
Ahmet Gümüşoluk <102166770+ahmetgumusoluk@users.noreply.github.com>
2023-06-01 20:29:38 +0300

    Create frmdoktorbilgiduzenle.cs

---
+++ frmdoktorbilgiduzenle.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Proje_hastane
+{
+    public partial class frmdoktorbilgiduzenle : Form
+    {
+        public frmdoktorbilgiduzenle()
+        {
+            InitializeComponent();
+        }
+        sqlbaglantisi bgl = new sqlbaglantisi();
+        public string TCNO;
+        private void frmdoktorbilgiduzenle_Load(object sender, EventArgs e)
+        {
+            msktc.Text = TCNO;
+            SqlCommand komut = new SqlCommand("select * from tbl_Doktorlar where DoktorTc=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", msktc.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+
+
+                txtad.Text = dr[1].ToString();
+                txtsoyad.Text = dr[2].ToString();
+                cmbbrans.Text = dr[3].ToString();
+                txtsifre.Text = dr[5].ToString();
+                 
+            }
+            bgl.baglanti().Close();
+        }
+
+        private void btnbilgiguncelle_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("update tbl_Doktorlar set DoktorAd=@p1 DoktorSoyad=@p2,DoktorBrans=@p3,DoktorSifre=@p4 where DoktorTc=@p5", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", txtad.Text);
+            komut.Parameters.AddWithValue("@p2", txtsoyad.Text);
+            komut.Parameters.AddWithValue("@p3", cmbbrans.Text);
+            komut.Parameters.AddWithValue("@p4", txtsifre.Text);
+            komut.Parameters.AddWithValue("@p5", msktc.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Kayıt güncellendi!");
+        }
+    }
+}
