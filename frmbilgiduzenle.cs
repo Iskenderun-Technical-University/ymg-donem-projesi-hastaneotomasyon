6179062bc533ae8ea7bb00df649cc2468d689e60
Ahmet Gümüşoluk <102166770+ahmetgumusoluk@users.noreply.github.com>
2023-06-01 20:28:56 +0300

    Create frmbilgiduzenle.cs

---
+++ frmbilgiduzenle.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Proje_hastane
+{
+    public partial class frmbilgiduzenle : Form
+    {
+        public frmbilgiduzenle()
+        {
+            InitializeComponent();
+        }
+        public string tcno;
+        sqlbaglantisi bgl = new sqlbaglantisi();
+        private void frmbilgiduzenle_Load(object sender, EventArgs e)
+        {
+            msktc.Text = tcno;
+            SqlCommand komut = new SqlCommand("select *from Tbl_Hastalar where HastaTc=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", msktc.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                txtad.Text = dr[1].ToString();
+                txtsoyad.Text = dr[2].ToString();
+                msktelefon.Text = dr[4].ToString();
+                txtsifre.Text = dr[5].ToString();
+                cmbcinsiyet.Text = dr[6].ToString();
+
+            }
+            bgl.baglanti().Close();
+        }
+
+        private void btnbilgiguncelle_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut2 = new SqlCommand("update Tbl_Hastalar set HastaAd=@p1,HastaSoyad=@p2,HastaTelefon=@p3,HastaŞifre=@p4,HastaCinsiyet=@p5 where HastaTC=@p6", bgl.baglanti());
+            komut2.Parameters.AddWithValue("@p1", txtad.Text);
+            komut2.Parameters.AddWithValue("@p2", txtsoyad.Text);
+            komut2.Parameters.AddWithValue("@p3", msktelefon.Text);
+            komut2.Parameters.AddWithValue("@p4", txtsifre.Text);
+            komut2.Parameters.AddWithValue("@p5", cmbcinsiyet.Text);
+            komut2.Parameters.AddWithValue("@p6", msktc.Text);
+
+            komut2.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("bilgieriniz güncellendi.", "bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
